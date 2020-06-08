6bf9522df12fd27f9023b72741ac13e647d2e5ab
32bitsret <dretnan@logicaladdress.com>
2020-06-09 00:49:14 +0100

    laravel specific addons

--- src/components/AppRow.jsx
+++ src/components/AppRow.jsx
@@ -10,7 +10,7 @@ class AppRow extends React.Component {
         <tr>
           <td><Link to={"/apps/" + this.props.data._id}>{this.props.data.app_name.toUpperCase()}</Link></td>
           <td>{this.props.data.template.toUpperCase()}/{this.props.data.template_variation.toUpperCase()}</td>
-          <td>{this.props.data.repo_full_name || (this.props.data.app_name == 'default' ? 'Default' : this.props.data.app_repository)}</td>
+          <td>{this.props.data.repo_full_name || (this.props.data.app_name === 'default' ? 'Default' : this.props.data.app_repository)}</td>
           <td style={
             this.props.data.state === "RUNNING" ? up : this.props.data.state === "LOADING" ? 
               check : down }>{this.props.data.state}
