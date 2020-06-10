2997191c82f9ec80cb78e7745988bd305b55d2ce
RETNAN DASER <retnan@RETNANs-iMac.local>
2020-06-10 20:30:38 +0100

    handling socket com for payments and fixing the stripe payment page for good predictability

--- src/components/SubHeader.jsx
+++ src/components/SubHeader.jsx
@@ -2,9 +2,35 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import req from '../api/req.js';
+import io from "socket.io-client";
 import {error, success} from '../utils/toastr';
+import constants from '../Constants';
+import { Link } from 'react-router-dom';
 
 class SubHeader extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            showPaymentFix: false,
+        };
+
+        this.socket = io.connect(constants.API_URL, {
+            query: {
+              token: req.getJwt(),
+              uid: req.getUid(),
+            }
+        });
+    }
+
+    componentDidMount(){
+        this.socket.on('PAYMENT_FAILED', (data) => {
+            this.setState({showPaymentFix: true});
+        });
+        this.socket.on('PAYMENT_SUCCESS', (data) => {
+            this.setState({showPaymentFix: false});
+        });
+    }
 
     resendConfirmationLink(e){
         e.preventDefault();
@@ -27,6 +53,7 @@ class SubHeader extends React.Component {
         return (
           <div>
             {this.props.profile.verified_email === false && <div className="white panel"><p className="lead">Check your email. You have not verified your mail. Kindly verify to have access to all pushdeploy features. <a href="" onClick={this.resendConfirmationLink}>Resend Link</a></p></div> }
+            {(this.props.profile.chargeFailed === true || this.state.showPaymentFix === true) && <div className="white panel"><p className="lead">There's a problem with your payment. <Link to={"/account/plans"}>Click to update your card</Link></p></div> }
             <div className="clear"></div>
         </div>     
         );
